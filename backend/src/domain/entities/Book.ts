75f2b7434b3c136bc4fa57467f4b9af15ae7b138
Lucas Gabriel Silva <lucasgsilva2102@gamil.com>
2022-05-06 10:57:11 -0300

    chore: config eslint

--- backend/src/domain/entities/Book.ts
+++ backend/src/domain/entities/Book.ts
@@ -1,11 +1,11 @@
 export class Book {
-  id: string;
-  title: string;
-  description: string;
-  author?: string;
-  publicationDate?: Date;
-  imagePath?: string;
-  filePath?: string;
+  id: string
+  title: string
+  description: string
+  author?: string
+  publicationDate?: Date
+  imagePath?: string
+  filePath?: string
 }
 
 export type CreateBookData = Omit<Book, 'id'>
