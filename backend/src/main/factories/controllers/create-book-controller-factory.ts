627a9e2a7c3f37bf40f8fb2f066361fdce936be5
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-05-13 17:19:52 -0300

    refactor: change book repository

--- backend/src/main/factories/controllers/create-book-controller-factory.ts
+++ backend/src/main/factories/controllers/create-book-controller-factory.ts
@@ -1,6 +1,6 @@
 import { CreateBookController } from '../../../application/controllers/book/create-book-controller'
 import { CreateBookUseCase } from '../../../application/usecases/book/create-book-usecase'
-import { PgCreateBookRepository } from '../../../infra/database/postgres/repositories/book-pg-repository'
+import { PgCreateBookRepository } from '../../../infra/database/postgres/repositories/create-book-pg-repository'
 import { LocalUploaderHelper } from '../../../infra/upload/local-uploader-helper'
 
 export const makeCreateBookController = () => {
