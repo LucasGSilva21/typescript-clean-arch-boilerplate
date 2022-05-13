afa5c9584663efff7b3e314ea474acab5abde148
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-05-13 17:22:26 -0300

    feat: add find all book postgres repository

---
+++ backend/src/infra/database/postgres/repositories/find-all-book-pg-repository.ts
@@ -0,0 +1,14 @@
+import { Book } from '../../../../domain/entities/book'
+import { FindAllRepository } from '../../../../application/protocols/repositories/book'
+import { PgRepository } from '../helpers/repository'
+import { PgBook } from '../entities/book'
+
+export class PgListAllBookRepository extends PgRepository implements FindAllRepository {
+  async findAll (): Promise<Book[]> {
+    const pgBookRepo = this.getRepository(PgBook)
+
+    const books = pgBookRepo.find()
+
+    return books
+  }
+}
--- backend/src/infra/database/postgres/repositories/index.ts
+++ backend/src/infra/database/postgres/repositories/index.ts
@@ -1,1 +1,2 @@
 export * from './create-book-pg-repository'
+export * from './find-all-book-pg-repository'
