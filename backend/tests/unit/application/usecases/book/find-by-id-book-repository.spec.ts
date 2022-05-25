5676695180d016fcd0104d83f5938ebe61519b66
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-05-25 12:07:23 -0300

    test: ensure FindByIdBookUseCase throw if FindByIdBookRepository throws

--- backend/tests/unit/application/usecases/book/find-by-id-book-repository.spec.ts
+++ backend/tests/unit/application/usecases/book/find-by-id-book-repository.spec.ts
@@ -2,6 +2,7 @@ import { FindByIdBookUseCase } from '../../../../../src/application/usecases/boo
 import { FindByIdBookRepository } from '../../../../../src/application/protocols/repositories/book'
 import { mockFindByIdBookRepository } from '../../../../utils/mocks/application'
 import { mockBook } from '../../../../utils/mocks/domain/mock-book'
+import { throwError } from '../../../../utils/helpers/throw-error'
 
 type SutTypes = {
   sut: FindByIdBookUseCase
@@ -29,5 +30,12 @@ describe('FindByIdBookUseCase', () => {
     const { sut } = makeSut()
     const books = await sut.findById('valid_id')
     expect(books).toEqual(mockBook())
+  })
+
+  test('Should throw if FindByIdBookRepository throws', async () => {
+    const { sut, findByIdBookRepository } = makeSut()
+    jest.spyOn(findByIdBookRepository, 'findById').mockImplementationOnce(throwError)
+    const promise = sut.findById('valid_id')
+    await expect(promise).rejects.toThrow()
   })
 })
