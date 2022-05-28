963b387f862eebaa4a4a3bb2eceb1386b0a3e710
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-05-28 12:50:26 -0300

    feat: add find by id book function

---
+++ backend/src/main/functions/book/find-by-id-book-function.ts
@@ -0,0 +1,22 @@
+import { PgConnection } from '../../../infra/database/postgres/helpers/connection'
+import { makeFindByIdBookController } from '../../factories/controllers'
+
+const findByIdBookController = makeFindByIdBookController()
+
+export const run = async (event: any) => {
+  try {
+    await PgConnection.getInstance().connect()
+    const { id } = event.pathParameters
+    const { statusCode, body } = await findByIdBookController.handle({ params: { id } })
+    return {
+      statusCode,
+      body: JSON.stringify(body)
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error'
+    }
+  }
+}
