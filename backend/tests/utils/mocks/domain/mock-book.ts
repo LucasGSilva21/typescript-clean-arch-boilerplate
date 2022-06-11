92b0690f2a8109239626e4f9d8c1b8b832ac9b4c
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-06-11 18:02:26 -0300

    refactor: change class to interface

--- backend/tests/utils/mocks/domain/mock-book.ts
+++ backend/tests/utils/mocks/domain/mock-book.ts
@@ -1,16 +1,14 @@
 import { Book, CreateBookData } from '../../../../src/domain/entities/book'
 
 export const mockBook = (): Book => {
-  const book = new Book()
-
-  book.id = 'book-id'
-  book.title = 'book-title'
-  book.description = 'book-description'
-  book.author = 'book-author'
-  book.publicationDate = new Date()
-  book.imagePath = 'book-image-path'
-
-  return book
+  return {
+    id: 'book-id',
+    title: 'book-title',
+    description: 'book-description',
+    author: 'book-author',
+    publicationDate: new Date(),
+    imagePath: 'book-image-path'
+  }
 }
 
 export const mockCreateBookData = (): CreateBookData => {
